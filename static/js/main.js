84471160446d7d9e59cf96ff23e3af24cb0db70d
Kirill Lukianov <bigfirestart@gmail.com>
2021-12-13 10:19:19 +0300

    feat: lab7 basic

--- static/js/main.js
+++ static/js/main.js
@@ -16,4 +16,26 @@ function scrollToContact() {
 contactRedirect.addEventListener('click', scrollToContact)
 
 
+// Pulse button
+contactRedirect.classList += ' animate__pulse animate__infinite'
+contactRedirect.style.setProperty('--animate-duration', '2s')
 
+
+// Modal
+let modalContent = $('#modalGithub')
+
+modal = new jBox('Modal', {
+  width: 500,
+  height: 250,
+  title: 'Have you seen my Github?',
+  content: modalContent,
+  onOpen: function () {
+    
+  }
+})
+
+$(window).on('load', function(){
+    setTimeout(function() {
+       modal.open()
+    }, 1 * 1000);
+});
